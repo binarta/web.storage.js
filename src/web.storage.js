8eea6da50742aec994b58b043e115d779325611b
Jo Kerkhofs <jke@thinkerit.be>
2017-04-07 11:03:07 +0200

    Made in-memory storage instances private

--- src/web.storage.js
+++ src/web.storage.js
@@ -1,27 +1,30 @@
-var InMemoryWebStorage = [];
+var WebStorageFactory = (function () {
+    var InMemoryWebStorage = [];
 
-function WebStorageFactory(storage) {
-    try {
-        window[storage].setItem('storageAvailable', true);
-        return window[storage];
-    } catch (ignored) {
-        if (!InMemoryWebStorage[storage]) InMemoryWebStorage[storage] = new InMemoryStorage();
-        return InMemoryWebStorage[storage];
-    }
+    return function (storage) {
+        try {
+            window[storage].setItem('storageAvailable', true);
+            return window[storage];
+        } catch (ignored) {
+            if (!InMemoryWebStorage[storage]) InMemoryWebStorage[storage] = new InMemoryStorage();
+            return InMemoryWebStorage[storage];
+        }
 
-    function InMemoryStorage() {
-        var self = this;
+        function InMemoryStorage() {
+            var self = this;
 
-        this.setItem = function (k, v) {
-            self[k] = '' + v
-        };
+            this.setItem = function (k, v) {
+                self[k] = '' + v
+            };
 
-        this.getItem = function (k) {
-            return self[k] || null;
-        };
+            this.getItem = function (k) {
+                return self[k] || null;
+            };
 
-        this.removeItem = function (k) {
-            delete self[k];
+            this.removeItem = function (k) {
+                delete self[k];
+            }
         }
     }
-}
+})();
+
